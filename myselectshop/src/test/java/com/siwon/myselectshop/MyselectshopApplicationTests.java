be90963474814f21886922f13d5637918bf8c35d
siwonkim <ktldnjs0811@naver.com>
2022-11-14 17:15:01 +0900

    myselectshop 프로젝트 생성

---
+++ myselectshop/src/test/java/com/siwon/myselectshop/MyselectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwon.myselectshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselectshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
