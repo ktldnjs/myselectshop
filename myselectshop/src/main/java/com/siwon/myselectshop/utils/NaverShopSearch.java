bee94f90eff8811ae076ee41a67e3a39e8263089
siwonkim <ktldnjs0811@naver.com>
2022-11-14 17:15:11 +0900

    자바로 네이버 쇼핑 API 이용하기

---
+++ myselectshop/src/main/java/com/siwon/myselectshop/utils/NaverShopSearch.java
@@ -0,0 +1,30 @@
+package com.siwon.myselectshop.utils;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class NaverShopSearch {
+    public String search() {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", "4mmKW0gcmBrhbbI7_nTd");
+        headers.add("X-Naver-Client-Secret", "Onhs6izL4k");
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아디다스", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        NaverShopSearch naverShopSearch = new NaverShopSearch();
+        naverShopSearch.search();
+    }
+}
+
