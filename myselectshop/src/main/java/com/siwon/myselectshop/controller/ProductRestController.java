5a20ec1a25f7c33225f72e44625474812672888d
siwonkim <ktldnjs0811@naver.com>
2022-11-14 17:59:58 +0900

    productController 생성 - 관심상품 조회기능

---
+++ myselectshop/src/main/java/com/siwon/myselectshop/controller/ProductRestController.java
@@ -0,0 +1,24 @@
+package com.siwon.myselectshop.controller;
+
+import com.siwon.myselectshop.models.Product;
+import com.siwon.myselectshop.models.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+// 관심상품 조회기능.
+@RestController // JSON 으로 응답
+@RequiredArgsConstructor // productController 를 사용할 때는 반드시 productRepository 가 있어야 된다고 스프링에게 알려주는 것.
+public class ProductRestController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping("/api/products")
+    public List<Product> readProducts() {
+        return productRepository.findAll();
+    }
+
+
+}
